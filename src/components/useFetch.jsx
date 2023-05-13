56ecad88a3e4e8e59f48b152536f55f8b23f44b7
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-13 04:33:50 +0100

    Create custom useFetch hook and add react router dependencies

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -9,7 +9,6 @@ const useFetch = (url) => {
   useEffect(() => {
     fetch(url)
       .then(res => {
-        console.log(res);
         if(!res.ok) {
           throw Error('Could not fetch the data for that resource');
         }
