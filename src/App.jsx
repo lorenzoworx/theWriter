b501d08884655e5f67a91c8651e95859dfdc9a0a
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-05-17 08:58:51 +0100

    implement routes for web pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/blogs/:id' element={<BlogDetails />} />
+          </Routes >
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
